639aed4d28aa16f0f33e4d3a184e6790feb7c6a5
mo <m@mh.com>
2023-09-04 05:53:32 +0300

    add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
     <>
       Hola from MH Team
     </>
   )
 }
-
-export default App
