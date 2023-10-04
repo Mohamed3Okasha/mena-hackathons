72c6ad1e3619f71c077b411db1b0642ccfd04ef4
mo <m@mh.com>
2023-10-04 23:07:27 +0300

    adjusthero section luminosity layover

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,15 +1,15 @@
 export function HeroSection(){
     return(
         <div 
-            className={`bg-blend-multiply bg-center`}
+            className={`bg-blend-luminosity bg-center`}
             style={{ 
                 backgroundImage: `url(https://github.com/Mohamed3Okasha/mena-hackathons/blob/main/src/assets/hero-section-bg.png?raw=true)`,
-                backgroundColor: `rgba(26, 67, 88, 0.60)`,
+                backgroundColor: `rgba(26, 67, 88, 1)`,
             }}> 
            <div className={`py-44 px-[80px] my-auto`}>
                 <h1 className="text-[#00FF95] text-6xl lg:text-8xl font-semibold leading-none mb-3 flex flex-col">
                     <span className={``} >
-                        MENA
+                        MENA 
                     </span> 
                     <span>
                        Hackathons
