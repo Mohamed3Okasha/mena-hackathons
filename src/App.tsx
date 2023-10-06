5ad102482ffe29f995f5a3aa75a65975cdcb573b
mo <m@mh.com>
2023-10-06 21:24:10 +0300

    add partners section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { HeroSection } from './components/HeroSection';
 import {Navbar} from './components/Navbar';
+import { PartnersSection } from './components/PartnersSection';
 import { ServicesSection } from './components/ServicesSection';
 
 export function App() {
@@ -9,6 +10,7 @@ export function App() {
     <Navbar />
     <HeroSection />
     <ServicesSection />
+    <PartnersSection />
     </>
   )
 }
---
+++ src/components/PartnersSection/index.tsx
@@ -0,0 +1,36 @@
+import accLogo from "../../assets/acc-logo.jpg";
+import hackIntlLogo from "../../assets/hackIntl-logo.jpg";
+import hackKosiceLogo from "../../assets/hackkosice-logo.jpg"
+
+export function PartnersSection(){
+    return(
+        <div>
+            <div className={`p-20 my-auto`}>
+                <div className={`mb-11`}>
+                    <h1 className="text-3xl lg:text-4xl font-semibold leading-none mb-3">
+                        <span className={`mr-3`} >
+                            Our
+                        </span> 
+                        <span className={`text-[#48ABDD]`}>
+                            Success Partners
+                        </span>
+                    </h1>
+                    <p className={`font-light text-1xl text-[#CECED0]`}>
+                        We worked together before, would love to see you with us
+                    </p>
+                </div>
+                <div className={`flex flex-col justify-center items-center gap-5 md:flex-row`}>
+                    <div>
+                        <img src={accLogo} alt="acc-logo" className={`max-w-sm`}/>
+                    </div>
+                    <div>
+                        <img src={hackIntlLogo} alt="acc-logo" className={`max-w-sm`}/>
+                    </div>
+                    <div>
+                        <img src={hackKosiceLogo} alt="acc-logo" className={`max-w-sm`}/>
+                    </div>
+                </div>
+             </div>
+        </div>
+    )
+}
