2c65fbecf182554bffd62a7d75ce219dcaf78bab
mo <m@mh.com>
2024-10-16 18:16:26 +0300

    comment unnecessary routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import { Home } from './pages/Home';
-import { SignPage } from './pages/Sign';
-import { Platform } from './pages/Platform';
+// import { SignPage } from './pages/Sign';
+// import { Platform } from './pages/Platform';
 import { Event } from './pages/Event';
 
 export function App() {
@@ -11,14 +11,14 @@ const router = createBrowserRouter([
     path: `/`,
     element: <Home />
   },
-  {
-    path: `/sign`,
-    element: <SignPage isSignUp = {false} />
-  },
-  {
-    path: `/app`,
-    element: <Platform />
-  },
+  // {
+  //   path: `/sign`,
+  //   element: <SignPage isSignUp = {false} />
+  // },
+  // {
+  //   path: `/app`,
+  //   element: <Platform />
+  // },
   {
     path: `/untapped`,
     element: <Event />
