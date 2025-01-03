70de268cded49f02bd670aadc79c92c0bee5dcc6
mo <m@mh.com>
2025-01-03 21:15:46 +0200

    implement on loadin circle home page,  bar, skeleton

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,48 @@
+export function CircleLoader({ size, icon }: { size: number, icon?: string }) {
+    const borderWidth = size / 30;
+
+    return (
+        <div className="flex items-center justify-center h-screen">
+            {icon && <img src={icon} alt={`mh logo`} className={`w-20`} />}
+            <div className={`absolute`}>
+                <div
+                    className=" animate-spin rounded-full border-primary"
+                    style={{
+                        width: `${size}px`,
+                        height: `${size}px`,
+                        borderWidth: `${borderWidth}px`,
+                        borderStyle: "solid",
+                        borderTopColor: "transparent",
+                    }}
+                />
+            </div>
+        </div>
+
+    )
+}
+
+export function LoadingBar({
+    width = "100%", 
+    height = "11px", 
+    color = "accent",
+  }: {width?: string, height?:string, color?:string}){
+    return (
+      <div
+        className="bg-accent relative" >
+        {/* Animated bar */}
+        
+        <div
+          className="absolute h-full bg-accent animate-pulse rounded"
+          style={{ backgroundColor: color, width, height }}
+        />
+      </div>
+    );
+}
+export function SkeletonLoader() {
+    return (<div className="space-y-4">
+        <div className="h-4 bg-accent rounded animate-pulse w-3/4"></div>
+        <div className="h-4 bg-accent rounded animate-pulse w-1/2"></div>
+        <div className="h-4 bg-accent rounded animate-pulse w-1/2
+        "></div>
+    </div>)
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,20 +1,45 @@
+import { useEffect, useState } from "react";
 import { DemoSection } from "../../components/DemoSection";
 import { Footer } from "../../components/Footer";
 import { HeroSection } from "../../components/HeroSection";
+import { CircleLoader } from "../../components/Loading";
 import { Navbar } from "../../components/Navbar";
 import { PartnersSection } from "../../components/PartnersSection";
 import { ProjectsSection } from "../../components/ProjectsSection";
 
-export function Home(){
-    return(
+import mhLogo from "../../assets/mh-logo-web.png"
+
+export function Home() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const handlePerformance = () => {
+            const perfEntries = performance.getEntriesByType("resource");
+            console.log("Loaded resources:", perfEntries);
+            setLoading(false);
+        };
+
+        window.addEventListener("load", handlePerformance);
+
+        return () => {
+            window.removeEventListener("load", handlePerformance);
+        };
+    }, []);
+
+    return (
         <>
-            <Navbar />
-            <HeroSection />
-            <ProjectsSection />
-            <DemoSection />
-            <PartnersSection />
-            <Footer />
-            
+            {loading ?
+                < CircleLoader size={120} icon={mhLogo} />
+                    :
+                <>
+                    <Navbar />
+                    <HeroSection />
+                    <ProjectsSection />
+                    <DemoSection />
+                    <PartnersSection />
+                    <Footer />
+                </>
+            }
         </>
     )
 }
