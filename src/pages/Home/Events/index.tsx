ac04f89c5c470f43075fbac6204d5d84ec4189ec
mo <m@mh.com>
2025-06-26 17:54:54 +0300

    update home page events

--- src/pages/Home/Events/index.tsx
+++ src/pages/Home/Events/index.tsx
@@ -1,7 +1,10 @@
 import { CollectionEvents } from "../../Platform/platformEvents";
 import eventImage from "../../../assets/event-card-image.png"
+import { useTranslation } from "react-i18next";
 
-export function HomePageEvents(){
+export function HomePageEvents() {
+    const [t, i18n] = useTranslation("global");
+
     const eventsData = [
         {
             imageUrl: eventImage,
@@ -12,25 +15,31 @@ export function HomePageEvents(){
             tags: [`Smart Cities`, `Sustainable Urbanisation`],
         },
         {
-        imageUrl: eventImage,
-        status: `upcoming`,
-        title: `HACKTIVE Youth`,
-        date: `2025-2026`, // format: "Month, Year"
-        location: `EU - SouthMed`, // format: "City, Country"
-        tags: [`Active Citizenship`],
-        },
-        {
             imageUrl: eventImage,
             status: `upcoming`,
-            title: `NEOM Competition`,
-            date: `2025`, // format: "Month, Year"
-            location: `Riyadh, KSA`, // format: "City, Country"
-            tags: [`Innovation`, `Entrepreneurship`],
+            title: `HACKTIVE Youth`,
+            date: `2025-2026`, // format: "Month, Year"
+            location: `EU - SouthMed`, // format: "City, Country"
+            tags: [`Active Citizenship`],
         },
+        // {
+        //     imageUrl: eventImage,
+        //     status: `upcoming`,
+        //     title: `NEOM Competition`,
+        //     date: `2025`, // format: "Month, Year"
+        //     location: `Riyadh, KSA`, // format: "City, Country"
+        //     tags: [`Innovation`, `Entrepreneurship`],
+        // },
     ]
-    return(
+    return (
         <div className={`my-11 flex flex-col gap-11`}>
-            <h1 className={`text-3xl font-semibold`}>Upcoming <span className={`text-primary`}>Events</span> </h1>
+            <h1 className={`text-3xl font-semibold`}>
+                {i18n.language === "ar" ? (
+                    <> {t("homePageEvents.titleEvents")} <span className={`text-primary`}>{t("homePageEvents.titleUpcoming")}</span> </>
+                ) : (
+                    <> {t("homePageEvents.titleUpcoming")} <span className={`text-primary`}>{t("homePageEvents.titleEvents")}</span> </>
+                )}
+            </h1>
             <CollectionEvents eventsData={eventsData} />
         </ div>
     )
